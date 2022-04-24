099d7b87169f9bd7b0a898bffafb6919008c9af0
Mokhtari Sofiane <mokhta_s@etna-alternance.net>
2022-04-24 23:39:34 +0200

    fix css presentation

--- pages/404.js
+++ pages/404.js
@@ -1,17 +1,17 @@
 import { Flex, Text, useColorModeValue, Icon } from "@chakra-ui/react";
-import React from "react";
 import Nav from "../components/Navbar";
 import { WarningIcon } from '@chakra-ui/icons'
 
 const NotFound = ({}) => {
   const bgColor = useColorModeValue("blue.50", "blue.800")
+  const color = useColorModeValue("black", "white")
 
   return (
       <>
         <Nav />
         <Flex bg={bgColor} h="100vh" justifyContent="center" alignItems="center">
             <Icon as={WarningIcon} colorScheme='black' variant='outline' padding="2" boxSize={150} />
-            <Text fontSize="7xl" color="primary.500" ml="35px">
+            <Text fontSize="6xl" color={color} ml="35px">
                 La page n'existe pas
             </Text>
         </Flex>
